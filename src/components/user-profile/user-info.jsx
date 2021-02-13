9360f47470acd9bde20b4db55d53a04c20f8cc94
Rob <robrichardsdpt@gmail.com>
2021-02-13 18:48:49 -0500

    started adding stats on the page

--- src/components/user-profile/user-info.jsx
+++ src/components/user-profile/user-info.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import PatchGrid from '../patch-grid/patch-grid'
+import './user-profile.styles.scss'
 
-const UserInfo = ({ user }) => {
+const UserInfo = ({ user, hikes }) => {
   return(
-    <div class='profile-information'>{user.email}
+    <div class='profile-information'>{user.email} <br/>
+      You have done {hikes.length} hikes!
     <PatchGrid user={user} />
     </div>
   )
