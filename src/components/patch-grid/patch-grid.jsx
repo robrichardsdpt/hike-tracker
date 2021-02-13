672622e1f2208ab960048ff3db46a844d353beda
Rob <robrichardsdpt@gmail.com>
2021-02-13 18:28:35 -0500

    Updated patch component

---
+++ src/components/patch-grid/patch-grid.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Patch from './patch'
+import './patch-grid.styles.scss'
+
+const PatchGrid = ({ user }) => {
+  return (
+    <div className='patch-grid'><Patch /></div>
+  )
+}
+
+export default PatchGrid
---
+++ src/components/patch-grid/patch.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Patch = ( {stats} ) => {
+  return(
+    <div>PATCH!</div>
+  )
+}
