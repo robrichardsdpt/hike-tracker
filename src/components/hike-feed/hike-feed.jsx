f0cef5929c627fe62bc91fccea7c554214a38368
Rob <robrichardsdpt@gmail.com>
2021-02-15 18:18:04 -0500

    edit modal works on the hike feed

--- src/components/hike-feed/hike-feed.jsx
+++ src/components/hike-feed/hike-feed.jsx
@@ -2,11 +2,20 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import apiUrl from '../../apiConfig.js'
 import HikeFeedCard from './hike-feed-card'
+import EditModal from '../edit-modal/edit-modal'
 import './hike-feed.styles.scss'
 
 const Feed = ({ user }) => {
   const [hikes, setHikes] = useState([])
   const [hikeId, setHikeId] = useState()
+  const [show, setShow] = useState(false)
+
+  const handleUpdateClick = (event) => {
+    setHikeId(event.target.id)
+    setShow(true)
+    console.log(event.target.id)
+  }
+  const handleClose = () => setShow(false) 
 
   useEffect(() => {
     axios({
@@ -17,18 +26,21 @@ const Feed = ({ user }) => {
       }
     })
     .then((res) => setHikes(res.data.hikes))
-  }, [])
+  }, [show])
 
   const hikesJsx = hikes.map(hike => {
     let profile
     return(
       <div key={hike._id}>
-      <HikeFeedCard hike={hike} user={user} profile={profile=false} setHikeId={setHikeId}/>
+      <HikeFeedCard hike={hike} user={user} profile={profile=false} setHikeId={setHikeId} handleUpdateClick={handleUpdateClick}/>
       </div>
     )
   })
   return(
-    <div className='hike-grid'>{hikesJsx}</div>
+    <div>
+      <div className='hike-grid'>{hikesJsx}</div>
+      {hikeId && <EditModal show={show} handleClose={handleClose} setHikeId={setHikeId} hikeId={hikeId} user={user}/> }
+    </div>
   )
 }
 
