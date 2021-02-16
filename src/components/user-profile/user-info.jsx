a1e5a03c9ebcf635689d330339fd1903d878253d
Rob <robrichardsdpt@gmail.com>
2021-02-16 16:32:16 -0500

    added styling to profile

--- src/components/user-profile/user-info.jsx
+++ src/components/user-profile/user-info.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import PatchGrid from '../patch-grid/patch-grid'
 import { hikeTotalDistance, hikeTotalElevation, mountainsClimbedToTable, mountainsTable } from '../../hike-data/hike-data-functions'
 import './user-profile.styles.scss'
+import NoPhoto from './no-photo-avail.jpg'
 
 const UserInfo = ({ user, hikes }) => {
   
@@ -9,7 +10,9 @@ const UserInfo = ({ user, hikes }) => {
   const totalDistance = hikeTotalDistance(hikes)
   const totalElevation = hikeTotalElevation(hikes)
   return(
-    <div className='profile-information'><h3>{user.email}</h3>
+    <div className='profile-information'>
+      {user.profileImage ? <div className='image-container'><img src={user.profileImage} className='profile-image'/></div> : <div className='image-container'><img src={NoPhoto} className='profile-image'/></div>}
+      <h3>{user.email}</h3>
       <h4>Hikes logged: {hikes.length}</h4>
       <h4>Total distance: {totalDistance} miles </h4>
       <h4>Total distance climbed: {totalElevation} feet</h4>
