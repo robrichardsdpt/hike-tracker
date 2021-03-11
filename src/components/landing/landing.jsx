557f34007bc9bc50c4b5d4268950c52a21d1d5d0
Rob <robrichardsdpt@gmail.com>
2021-03-10 19:52:07 -0500

    fixed landing to instruct about new england tracking

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -4,7 +4,7 @@ import './landing.styles.scss'
 const Landing = () => {
   return(
     <div className='landing__container'>
-      <h1>welcome.</h1>
+      <h1>Welcome to your premier New England hike tracker!</h1>
       <h3>Sign up/in to start tracking your hikes and collecting patches!</h3>
     </div>
   )
