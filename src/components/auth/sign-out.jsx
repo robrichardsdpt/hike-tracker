3d43fdd4dc7532cb86113fa938493c742492232a
Rob <robrichardsdpt@gmail.com>
2022-03-28 11:05:47 -0400

    cleaned up unused variables

--- src/components/auth/sign-out.jsx
+++ src/components/auth/sign-out.jsx
@@ -1,25 +1,26 @@
-import { Component } from 'react'
-import { withRouter } from 'react-router-dom'
-import { deleteMountainsTable } from './../../hike-data/hike-data-functions'
-import { signOut } from './auth-functions'
-import messages from '../alerts/messages'
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { signOut } from "./auth-functions";
+import messages from "../alerts/messages";
 
 class SignOut extends Component {
-  componentDidMount () {
-    const { msgAlert, history, clearUser, user } = this.props
+  componentDidMount() {
+    const { msgAlert, history, clearUser, user } = this.props;
     signOut(user)
-      .finally(() => msgAlert({
-        heading: 'Signed Out Successfully',
-        message: messages.signOutSuccess,
-        variant: 'success'
-      }))
-      .finally(() => history.push('/'))
-      .finally(() => clearUser())
+      .finally(() =>
+        msgAlert({
+          heading: "Signed Out Successfully",
+          message: messages.signOutSuccess,
+          variant: "success",
+        })
+      )
+      .finally(() => history.push("/"))
+      .finally(() => clearUser());
   }
 
-  render () {
-    return ''
+  render() {
+    return "";
   }
 }
 
-export default withRouter(SignOut)
+export default withRouter(SignOut);
--- src/components/auth/sign-up.jsx
+++ src/components/auth/sign-up.jsx
@@ -1,56 +1,59 @@
-import React, { Component } from 'react'
-import { withRouter } from 'react-router-dom'
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-import { signUp, signIn } from './auth-functions'
-import messages from '../alerts/messages.js'
+import { signUp, signIn } from "./auth-functions";
+import messages from "../alerts/messages.js";
 
-import Form from 'react-bootstrap/Form'
-import './sign-up.styles.scss'
+import Form from "react-bootstrap/Form";
+import "./sign-up.styles.scss";
 
 class SignUp extends Component {
-  constructor () {
-    super()
+  constructor() {
+    super();
 
     this.state = {
-      firstName: '',
-      lastName: '',
-      role: '',
-      email: '',
-      password: '',
-      passwordConfirmation: ''
-    }
+      firstName: "",
+      lastName: "",
+      role: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+    };
   }
 
-  handleChange = event => this.setState({
-    [event.target.name]: event.target.value
-  })
+  handleChange = (event) =>
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
 
-  onSignUp = event => {
-    event.preventDefault()
+  onSignUp = (event) => {
+    event.preventDefault();
 
-    const { msgAlert, history, setUser } = this.props
+    const { msgAlert, history, setUser } = this.props;
 
     signUp(this.state)
       .then(() => signIn(this.state))
-      .then(res => setUser(res.data.user))
-      .then(() => msgAlert({
-        heading: 'Sign Up Success',
-        message: messages.signUpSuccess,
-        variant: 'success'
-      }))
-      .then(() => history.push('/home'))
-      .catch(error => {
-        this.setState({ email: '', password: '', passwordConfirmation: '' })
+      .then((res) => setUser(res.data.user))
+      .then(() =>
         msgAlert({
-          heading: 'Sign Up Failed with error: ' + error.message,
-          message: messages.signUpFailure,
-          variant: 'danger'
+          heading: "Sign Up Success",
+          message: messages.signUpSuccess,
+          variant: "success",
         })
-      })
-  }
+      )
+      .then(() => history.push("/home"))
+      .catch((error) => {
+        this.setState({ email: "", password: "", passwordConfirmation: "" });
+        msgAlert({
+          heading: "Sign Up Failed with error: " + error.message,
+          message: messages.signUpFailure,
+          variant: "danger",
+        });
+      });
+  };
 
-  render () {
-    const { firstName, lastName, role, email, password, passwordConfirmation } = this.state
+  render() {
+    const { email, password, passwordConfirmation } = this.state;
 
     return (
       <div className="row sign-up">
@@ -90,17 +93,14 @@ class SignUp extends Component {
                 onChange={this.handleChange}
               />
             </Form.Group>
-            <button
-              className="sign-up-button"
-              type="submit"
-            >
+            <button className="sign-up-button" type="submit">
               Submit
             </button>
           </Form>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default withRouter(SignUp)
+export default withRouter(SignUp);
