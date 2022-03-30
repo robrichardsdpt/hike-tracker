0373452840828c012dcbe8c55525ab2456654c64
Rob <robrichardsdpt@gmail.com>
2022-03-30 10:41:39 -0400

    cleaned up helper function

--- src/components/hike-feed/helper.js
+++ src/components/hike-feed/helper.js
@@ -1,16 +1,19 @@
 export const filterData = (searchContents, hikes) => {
   let searchContentsLowerCase = searchContents.toLowerCase();
-  const filterData = hikes.filter(
-    (item) =>
-      item.mountainsClimbed?.toLowerCase().includes(searchContentsLowerCase) ||
-      item.trails.toLowerCase().includes(searchContentsLowerCase) ||
-      item.hikedWith?.toLowerCase().includes(searchContentsLowerCase) ||
-      item.trailNotes?.toLowerCase().includes(searchContentsLowerCase) ||
-      item.owner.email.toLowerCase().includes(searchContentsLowerCase)
-  );
-  return filterData.sort((a, b) => {
-    let aa = a.date.split("/").reverse().join(),
-      bb = b.date.split("/").reverse().join();
-    return bb < aa ? -1 : bb > aa ? 1 : 0;
-  });
+  return hikes
+    .filter(
+      (item) =>
+        item.mountainsClimbed
+          ?.toLowerCase()
+          .includes(searchContentsLowerCase) ||
+        item.trails.toLowerCase().includes(searchContentsLowerCase) ||
+        item.hikedWith?.toLowerCase().includes(searchContentsLowerCase) ||
+        item.trailNotes?.toLowerCase().includes(searchContentsLowerCase) ||
+        item.owner.email.toLowerCase().includes(searchContentsLowerCase)
+    )
+    .sort((a, b) => {
+      let aa = a.date.split("/").reverse().join(),
+        bb = b.date.split("/").reverse().join();
+      return bb < aa ? -1 : bb > aa ? 1 : 0;
+    });
 };
