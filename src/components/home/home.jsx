5bd1b40e2e85a6e4c2f1945c4f6e085336a8bbfe
Rob <robrichardsdpt@gmail.com>
2022-03-30 10:45:06 -0400

    removed unnecessary import

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import CreateHike from '../create-hike/create-hike'
-import './home.styles.scss'
-import Feed from './../hike-feed/hike-feed'
+import React from "react";
+import "./home.styles.scss";
+import Feed from "./../hike-feed/hike-feed";
 
 const Home = ({ user, msgAlert }) => {
   return (
-    <div className='home'>
-      <Feed user={user}/>
+    <div className="home">
+      <Feed user={user} />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
