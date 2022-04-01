c594ea2eafe37635aa68eb16e2c8f54bffa1dd41
Rob <robrichardsdpt@gmail.com>
2022-04-01 14:26:37 -0400

    made the landing screen more responsive.

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import './landing.styles.scss'
+import React from "react";
+import "./landing.styles.scss";
 
 const Landing = () => {
-  return(
-    <div className='landing__container'>
-      <h1>Welcome to your premier New England hike tracker!</h1>
-      <h3>Sign up/in to start tracking your hikes and collecting patches!</h3>
+  return (
+    <div className="landing__container">
+      <h1 className="landing__title">
+        Welcome to your premier New England hike tracker!
+      </h1>
+      <div className="landing__subtitle">
+        Sign up/in to start tracking your hikes and collecting patches!
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
